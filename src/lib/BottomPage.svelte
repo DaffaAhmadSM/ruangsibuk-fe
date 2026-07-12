4d622854be9a4ffa2034347893f8265e66531fca
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2026-07-13 02:01:54 +0700

    linking

--- src/lib/BottomPage.svelte
+++ src/lib/BottomPage.svelte
@@ -13,8 +13,12 @@
 
 	<!-- Centered content -->
 	<div class="relative z-10 flex flex-col items-center gap-6 md:flex-row md:gap-8">
-		<img src="/anlogo.webp" alt="Ruang Sibuk logo" class="w-[28vw] max-w-41.75 md:w-41.75" />
-		<img src="/daflogo.webp" alt="" class="w-[28vw] max-w-41.75 md:w-41.75" />
+    <a href="https://www.instagram.com/antaraaa.a/" target="_blank" rel="noopener noreferrer">
+      <img src="/anlogo.webp" alt="Ruang Sibuk logo" class="w-[28vw] max-w-41.75 md:w-41.75" />
+    </a>
+    <a href="https://www.instagram.com/dafra_aa/" target="_blank" rel="noopener noreferrer">
+      <img src="/daflogo.webp" alt="DAF logo" class="w-[28vw] max-w-41.75 md:w-41.75" />
+    </a>
 	</div>
 
 	<!-- Bottom icon -->
